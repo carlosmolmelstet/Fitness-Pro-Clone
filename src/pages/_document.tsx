4d5ae0ef8ab547cfcb37e910e9c7f436996701a9
Carlos Molmelstet <molmelstet2016@gmail.com>
2021-10-18 21:09:03 -0300

    font https

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,7 +13,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link rel="shortcut icon" href="/assets/images/favicon.ico" />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
-          <link href="http://fonts.cdnfonts.com/css/sf-pro-display" rel="stylesheet" />
+          <link href="https://fonts.cdnfonts.com/css/sf-pro-display" rel="stylesheet" />
 
           <Title>Fitness Landing</Title>
         </Head>
